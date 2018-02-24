af1ff1c0ecee3d94cbf89f59e0cea078b1f4b34b
CJBourassa <36799177+CJBourassa@users.noreply.github.com>
2018-02-24 14:55:24 -0500

    Update Add Two Digits.php
    Formatting

--- Add Two Digits.php
+++ Add Two Digits.php
@@ -1,24 +1,24 @@
 <?php
 // You are given a two-digit integer n. Return the sum of its digits.
-// 
+
 // Example
-//      For n = 29, the output should be
-//      addTwoDigits(n) = 11.
-//      Input/Output
-//      
+// For n = 29, the output should be
+// addTwoDigits(n) = 11.
+
 // [execution time limit] 4 seconds (php)
-// 
+
 // [input] integer n
-//      A positive two-digit integer.
-//      Guaranteed constraints:
-//      10 ≤ n ≤ 99.
-//      
+// A positive two-digit integer.
+// Guaranteed constraints:
+// 10 ≤ n ≤ 99.
+
 // [output] integer
-//      The sum of the first and second digits of the input number.
+// The sum of the first and second digits of the input number.
 
 function addTwoDigits($n) {
-// Turn the two digit number into a string
-$n = strval($n);
+
+$n = strval($n); // Turn the two digit number into a string
+  
 // Strings are stored as arrays in PHP, 
 // so read each digit as your would if 
 // they were stored in an array;
