d852b6eabb125ef330ef1c1e5db8da058ab1d017
CJBourassa <36799177+CJBourassa@users.noreply.github.com>
2018-03-15 14:21:01 -0400

    Update Different Rightmost Bit.php

--- The Core/Corner of 0s and 1s/Different Rightmost Bit.php
+++ The Core/Corner of 0s and 1s/Different Rightmost Bit.php
@@ -1,7 +1,6 @@
 <?php
-/* You're given two integers, n and m. Find position of the rightmost bit in which they differ in their binary representations (it is guaranteed that such a bit exists), counting from right to left.
-Return the value of 2position_of_the_found_bit (0-based)./*
- */
+
+// https://codefights.com/arcade/code-arcade/corner-of-0s-and-1s/whz5JzszYTdXW6aNA
 
 function differentRightmostBit($n, $m) {
   return ($n ^ $m) & -($n ^ $m);
