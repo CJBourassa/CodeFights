e2067714106330ddd1bec5955591114c8ac93e23
CJBourassa <36799177+CJBourassa@users.noreply.github.com>
2018-03-15 14:04:41 -0400

    Update Extra Number.php

--- The Core/At the Crossroads/Extra Number.php
+++ The Core/At the Crossroads/Extra Number.php
@@ -1,35 +1,8 @@
 <?php
-//You're given three integers, a, b and c. It is guaranteed that two of these 
-//integers are equal to each other. What is the value of the third integer?
 
-//Example
-//For a = 2, b = 7 and c = 2, the output should be
-//extraNumber(a, b, c) = 7.
-//The two equal numbers are a and c. The third number (b) equals 7, 
-//which is the answer.
-
-//[execution time limit] 4 seconds (php)
-
-//[input] integer a
-//Guaranteed constraints:
-//1 ≤ a ≤ 109.
-
-//[input] integer b
-//Guaranteed constraints:
-//1 ≤ b ≤ 109.
-
-//[input] integer c
-//Guaranteed constraints:
-//1 ≤ c ≤ 109.
-
-//[output] integer
-
-
+// https://codefights.com/arcade/code-arcade/at-the-crossroads/sgDWKCFQHHi5D3Szj/solutions
 
 function extraNumber($a, $b, $c) {
-    if ($a == $b)
-        return $c;
-    if ($b == $c)
-        return $a;
-    return $b;
+    // return the value that only appears once in an array containing a, b and c
+    return array_search(1, array_count_values([$a, $b, $c]));
 }
