5065e3a54b5299f7c7d6296124de3e5d66fa0519
CJBourassa <36799177+CJBourassa@users.noreply.github.com>
2018-03-15 13:38:59 -0400

    Update Candies.php

--- The Core/Intro Gates/Candies.php
+++ The Core/Intro Gates/Candies.php
@@ -1,4 +1,7 @@
 <?php
+
+// https://codefights.com/arcade/code-arcade/intro-gates/DdNKFA3XCX6XN7bNz
+
 function candies($n, $m) {
     // floor($m / $n) represents the number of candies that each child can get
     // Multiply this value by the number of children to get the total number of 
