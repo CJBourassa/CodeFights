e03eabe3d49522024f5078b70e67bb8ff693ddb3
CJBourassa <36799177+CJBourassa@users.noreply.github.com>
2018-03-15 13:41:41 -0400

    Update Circle of Numbers.php

--- The Core/Intro Gates/Circle of Numbers.php
+++ The Core/Intro Gates/Circle of Numbers.php
@@ -1,5 +1,7 @@
 <?php
-   
+ 
+// https://codefights.com/arcade/code-arcade/intro-gates/vExYvcGnFsEYSt8nQ
+
 function circleOfNumbers($n, $firstNumber) {
     // opposite number from $firstNumber is found by adding it to
     // the radius and then taking the modulus to account for when
