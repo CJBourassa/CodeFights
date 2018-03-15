71f2d560551ef4619106335d10db6d79b68f0298
CJBourassa <36799177+CJBourassa@users.noreply.github.com>
2018-03-15 14:05:27 -0400

    Update Is Infinite Process.php

--- The Core/At the Crossroads/Is Infinite Process.php
+++ The Core/At the Crossroads/Is Infinite Process.php
@@ -1,40 +1,13 @@
 <?php
 
-//Given integers a and b, determine whether the following 
-//pseudocode results in an infinite loop
-
-//while a is not equal to b do
-  //increase a by 1
-  //decrease b by 1
-
-//Assume that the program is executed on a virtual machine which can store 
-//arbitrary long numbers and execute forever.
-
-//Example
-//For a = 2 and b = 6, the output should be
-//isInfiniteProcess(a, b) = false;
-//For a = 2 and b = 3, the output should be
-//isInfiniteProcess(a, b) = true.
-
-//[execution time limit] 4 seconds (php)
-
-//[input] integer a
-//Guaranteed constraints:
-//0 ≤ a ≤ 20.
-
-//[input] integer b
-//Guaranteed constraints:
-//0 ≤ b ≤ 20.
-
-//[output] boolean
-//true if the pseudocode will never stop, false otherwise.
+// https://codefights.com/arcade/code-arcade/at-the-crossroads/aFF9HDm2Rsti9j5kc/solutions
 
 function isInfiniteProcess($a, $b) {
-// true if...
-// $a is even and $b is odd
-// or
-// $b is even and $a is odd
-// or
-// $a > $b 
-return !($a%2) && $b%2 || !($b%2) && $a%2 || $a > $b;
+    // true if...
+    // $a is even and $b is odd
+    // or
+    // $b is even and $a is odd
+    // or
+    // $a > $b 
+    return !($a%2) && $b%2 || !($b%2) && $a%2 || $a > $b;
 }
